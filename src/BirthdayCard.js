ae125687106bb3ef973f578d1c49212b0cec147b
codeChrisM <marlowe.animation@gmail.com>
2023-04-01 11:06:32 -0400

    find path debug

--- src/BirthdayCard.js
+++ src/BirthdayCard.js
@@ -18,6 +18,10 @@ function BirthdayCard() {
       <div className={`birthday-card${opened ? " opened" : ""}`}>
         <div className="front">
           <img src="../img/Runner1.jpg" alt="runner close up"></img>
+          <img src="./src/img/Runner1.jpg" alt="runner close up"></img>
+          <img src="src/img/Runner1.jpg" alt="runner close up"></img>
+          <img src="../../src/img/Runner1.jpg" alt="runner close up"></img>
+          <img src=".././src/img/Runner1.jpg" alt="runner close up"></img>
           <button onClick={handleClick}>Open</button>
           <h1>Happy Birthday</h1>
         </div>
